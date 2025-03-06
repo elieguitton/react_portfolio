babf15ef6cd5cbc2f3b5bed48064f083d8eccb98
Elie Guitton <guitton.elie@aol.fr>
2025-03-06 09:12:40 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Navbar from './components/Navbar'
+
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl front-bold underline'>Hello World !</h1>
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className='flixed top-0 --z-10 h-full w-full'></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
+      
+      <div className='container mx-auto px-8'>
+        <Navbar />  
+      </div>
     </div>
   )
 }
