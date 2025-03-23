3367b10f85ee62f8e0816eb3953df9e1c15016bc
eguitton <guitton.elie@aol.fr>
2025-03-23 11:23:59 +0100

    exepriences

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,68 +12,61 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "01/2022 - 11/2024",
+    role: "Freelance",
+    company: "Freelance",
+    description: ``,
+    technologies: ["React.js", "TypeScript", "MySQL", "Node.js"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "03/2024 - 10/2024",
+    role: "Responsable Service Informatique",
+    company: "OTC Royan Atlantique",
+    description: ``,
+    technologies: ["Unify", "O365", "Intune", "Entra ID"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    year: "09/2021 - 09/2023",
+    role: "Développeur Full-Stack",
+    company: "ADEI17",
+    description: ``,
+    technologies: ["PHP", "JavaScript", "SQLSever", "Symfony","Powershell"],
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "04/2021 - 06/2021",
+    role: "Développeur Web",
+    company: "Bernezac Communication",
+    description: ``,
+    technologies: ["HTML", "PHP", "JavaScript", "Postgres"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Master - Architecte Logiciel (2023)",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+      "",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
-    image: project2,
+    title: "Licence Informatique (2021)",
+    image: project1,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Portfolio Website",
-    image: project3,
+    title: "Bac Scientifique - SVT (2018)",
+    image: project1,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
 ];
 
 export const CONTACT = {
   address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  phoneNo: "06 51 47 75 99",
+  email: "guitton.elie@aol.fr",
 };
